c0d78730522e19f7ad284f35b1bf49d5189b78aa
Troi <tverg419@gmail.com>
2021-10-17 16:48:16 -0400

    added Tasks seed

---
+++ src/app/seed-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task'
+
+export const Tasks: Task[] = [
+    {
+        id: 1,
+        text: 'Learn Angular',
+        day: 'Sunday',
+        reminder: false,
+    },
+    {
+        id: 2,
+        text: 'Job Hunt',
+        day: 'Monday',
+        reminder: false,
+    },
+    {
+        id: 3,
+        text: 'Networking',
+        day: 'Monday',
+        reminder: true,
+    }
+]
